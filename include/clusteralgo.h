85a96278eb106d0d86b7e1fe809bf0c1c0190891
zackofira <150864953+zackofira@users.noreply.github.com>
2024-08-01 01:56:04 -0500

    Update clusteralgo.h

--- include/clusteralgo.h
+++ include/clusteralgo.h
@@ -71,6 +71,10 @@ private:
 
     void assign_noncore_hubs_outliers();
 
+    std::set<std::string> tokenize(const std::string& str);
+    bool calculateSharedPercentage(const std::set<std::string>& set1, const std::set<std::string>& set2);
+    bool semantic_similarity_check(int u, int v);
+
 };
 
 
